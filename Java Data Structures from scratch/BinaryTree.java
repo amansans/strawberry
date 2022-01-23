0f5654d780364965bbdddf1ebe43dcca44a61bc6
Aman Sansowa <88630319+amansans@users.noreply.github.com>
2022-01-23 10:40:56 -0500

    Add files via upload

---
+++ Java Data Structures from scratch/BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree {
+    // Tree (node)
+    // Node (value, leftChild, rightChild)
+    // insert (value)
+    // find (value):boolean
+
+    private class BinaryNode {
+        private int value;
+        private BinaryNode leftChild;
+        private BinaryNode rightChild;
+
+        public BinaryNode(int value) {
+            this.value = value;
+        }
+    }
+}
