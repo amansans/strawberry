5736a5dee8643b3e0c3f8c45ce7d447688fb4312
Aman Sansowa <88630319+amansans@users.noreply.github.com>
2022-03-25 13:13:46 -0400

    Add files via upload

---
+++ Java Application using microservices and cloud config/department-service/src/main/java/com/basic_micro/department/entity/Department.java
@@ -0,0 +1,26 @@
+package com.basic_micro.department.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
