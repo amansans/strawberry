dfa7fde9661bc33a28ff144e9a4f51d644bdff0f
Aman Sansowa <88630319+amansans@users.noreply.github.com>
2022-03-25 13:14:01 -0400

    Add files via upload

---
+++ Java Application using microservices and cloud config/hystrix-dashboard/src/test/java/com/basic_micro/hystrix/dashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,15 @@
+package com.basic_micro.hystrix.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
